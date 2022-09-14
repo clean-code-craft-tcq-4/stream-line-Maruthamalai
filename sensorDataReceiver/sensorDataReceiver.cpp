5219650522cdb206fe7b0fa6a324b9c52fdfb2a7
Yuvaraj-Ayyasamy <105203963+Yuvaraj-Ayyasamy@users.noreply.github.com>
2022-09-14 10:58:11 +0530

    Create sensorDataReceiver.cpp

---
+++ sensorDataReceiver/sensorDataReceiver.cpp
@@ -0,0 +1,17 @@
+#include <cstring>
+#include <cstdio>
+#include <iostream>
+#include "../batterySensors/chargeStatus/chargeStatus.hpp"
+#include "../batterySensors/temperature/temperature.hpp"
+#include <vector>
+#include "sensorDataSender.hpp"
+
+using namespace std;
+
+void readDataFromConsole(int pip[])
+{
+    char writeBuffer[1000];
+    memset(writeBuffer, 0, sizeof(writeBuffer));
+    read(pip[0], writeBuffer, 450);
+    cout<<writeBuffer;
+}
