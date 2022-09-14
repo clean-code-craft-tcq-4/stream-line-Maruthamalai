289b4a72bbd3c5e7d0184323980250195968d9b6
Yuvaraj-Ayyasamy <105203963+Yuvaraj-Ayyasamy@users.noreply.github.com>
2022-09-15 01:29:09 +0530

    Update sensorDataSender.cpp

--- sensorDataSender/sensorDataSender.cpp
+++ sensorDataSender/sensorDataSender.cpp
@@ -1,6 +1,4 @@
 #include <cstring>
-//#include <cstdio>
-//#include <cstdlib>
 #include <iostream>
 #include <sys/types.h>
 #include <sys/wait.h>
@@ -14,26 +12,23 @@ using namespace std;
 
 int writeDataToConsole(int pip[])
 {
-  int writeBuffSize = 0;
-  char writeBuffer[550];
-  memset(writeBuffer, '\0', sizeof(writeBuffer));
-  vector<int> currentListInAmps = getCurrentListInAmps(AMP_MIN, AMP_MAX, WRITE_COUNT);
-  vector<int> temperatureListInCelcious = getTemperatureInCelcious(TEMPERATURE_MIN_VALUE, TEMPERATURE_MAX_VALUE, WRITE_COUNT);
+    int writeBuffSize = 0;
+    char writeBuffer[550];
+    memset(writeBuffer, '\0', sizeof(writeBuffer));
+    vector<int> currentListInAmps = getCurrentListInAmps(AMP_MIN, AMP_MAX, WRITE_COUNT);
+    vector<int> temperatureListInCelcious = getTemperatureInCelcious(TEMPERATURE_MIN_VALUE, TEMPERATURE_MAX_VALUE, WRITE_COUNT);
 
-  if ((currentListInAmps.size() == WRITE_COUNT) && (temperatureListInCelcious.size() == WRITE_COUNT))
-  {
-    for (int counter = 0; counter < WRITE_COUNT; counter++)
+    if ((currentListInAmps.size() == WRITE_COUNT) && (temperatureListInCelcious.size() == WRITE_COUNT))
     {
-      char tempBuffer[11];
-      //cout << currentListInAmps[counter] << ", " << temperatureListInCelcious[counter] << endl;
-      memset(tempBuffer, '\0', sizeof(tempBuffer));
-      sprintf(tempBuffer, "%d,%d\n", currentListInAmps[counter], temperatureListInCelcious[counter]);
-      strcat(writeBuffer, tempBuffer);
+        for (int counter = 0; counter < WRITE_COUNT; counter++)
+        {
+            char tempBuffer[11];
+            memset(tempBuffer, '\0', sizeof(tempBuffer));
+            sprintf(tempBuffer, "%d,\t%d\n", currentListInAmps[counter], temperatureListInCelcious[counter]);
+            strcat(writeBuffer, tempBuffer);
+        }
+        writeBuffSize = strlen(writeBuffer);
+        write(pip[1], writeBuffer, writeBuffSize);
     }
-    write(pip[1], writeBuffer, strlen(writeBuffer));
-    cout << writeBuffer;
-    writeBuffSize = strlen(writeBuffer);
-  }
-
-  return writeBuffSize;
+    return writeBuffSize;
 }
