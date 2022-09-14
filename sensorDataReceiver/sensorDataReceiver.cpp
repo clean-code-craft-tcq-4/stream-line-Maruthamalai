c76bc08cf706f749d4b765038e4e0999de43a062
Yuvaraj-Ayyasamy <105203963+Yuvaraj-Ayyasamy@users.noreply.github.com>
2022-09-14 23:05:03 +0530

    Update sensorDataReceiver.cpp

--- sensorDataReceiver/sensorDataReceiver.cpp
+++ sensorDataReceiver/sensorDataReceiver.cpp
@@ -1,13 +1,10 @@
 #include <cstring>
-#include <cstdio>
-#include <cstdlib>
-#include <iostream>
+//#include <cstdio>
+//#include <cstdlib>
+//#include <iostream>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
-#include "../batterySensors/chargeStatus/chargeStatus.hpp"
-#include "../batterySensors/temperature/temperature.hpp"
-#include <vector>
 
 using namespace std;
 
