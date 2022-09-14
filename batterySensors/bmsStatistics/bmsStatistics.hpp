f0d41eec3559b071f1a425c9bdb85b9de6555399
Yuvaraj-Ayyasamy <105203963+Yuvaraj-Ayyasamy@users.noreply.github.com>
2022-09-15 00:35:12 +0530

    Update bmsStatistics.hpp

--- batterySensors/bmsStatistics/bmsStatistics.hpp
+++ batterySensors/bmsStatistics/bmsStatistics.hpp
@@ -1,5 +1,13 @@
 #pragma once
 
 #define WRITE_COUNT  (50)
+#define MIN_RANGE     0
+#define MAX_RANGE     WRITE_COUNT
 
+int minBmsData(int bmsList[]);
+int maxBmsData(int bmsList[]);
+void sortBmsData(int currList[], int tempList[]);
+void movingAvgOfBmsData(int currList[], int tempList[]);
+void bmsDataCharToInt(char writeBuffer[], int buffSize, int bmsDataList[]);
+void splitBmsData(int bmsDataList[], int currList[], int tempList[]);
 void bmsDataStatistics(char writeBuffer[], int buffSize);
